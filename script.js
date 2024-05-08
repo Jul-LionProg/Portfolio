931aaeb53affb69d2d622fe66f704e759a7233f0
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-07 21:37:36 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ $(document).ready(function(){
                 items: 2,
                 nav: false
             },
-            100:{
+            1000:{
                 items: 3,
                 nav: false
             }
